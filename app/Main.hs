4c9834bd49a8a8f7c30a6d87c7228ee6ed60de1c
bielrossi15 <bielrossiborba@gmail.com>
2020-09-17 03:31:28 -0300

    Adding jsonObjectParser and helper functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import JsonParser
+
+parseFile :: FilePath -> Parser a -> IO (Maybe a)
+parseFile file parser = do
+    input <- readFile file
+    return (snd <$> runParser parser input) 
 
 main :: IO ()
 main = undefined
